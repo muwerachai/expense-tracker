73f7da94b0edac8e722c3752318b713c01a98086
werachai <>
2024-04-29 17:33:05 +0700

    binding state to input

--- src/App.js
+++ src/App.js
@@ -29,89 +29,145 @@ const initialTransactions = [
   }
 ];
 
+const expenses = ["Food", "Grocery", "Health", "Transport", "Utilities"];
+const incomes = ["Investment", "Lotto", "Salary", "Wage"];
+
 function App() {
   const [transactions, setTransactions] = useState(initialTransactions);
   const [payee, setPayee] = useState("");
-  const [category, setCategory] = useState("");
+  const [category, setCategory] = useState("Food");
   const [amount, setAmount] = useState("");
   const [date, setDate] = useState("");
-  const [type, setType] = useState("");
+  const [type, setType] = useState("Expense");
+  const [isShowCreateForm, setIsShowCreateForm] = useState(false);
 
   return (
     <div className="container" style={{ maxWidth: 768 }}>
-      {/* ********** Begin Create Form ********** */}
-      <div className="bg-white p-3 rounded-2 my-3">
-        <form className="row g-3">
-          {/* ********** Begin Radio Button: Expense or Income ********** */}
-          <div className="col-12">
-            <div className="btn-group">
-              <input
-                type="radio"
-                className="btn-check"
-                id="cbx-expense"
-                name="type"
-                defaultChecked
-              />
-              <label className="btn btn-outline-danger" htmlFor="cbx-expense">
-                Expense
-              </label>
-              <input
-                type="radio"
-                className="btn-check"
-                id="cbx-income"
-                name="type"
-              />
-              <label className="btn btn-outline-success" htmlFor="cbx-income">
-                Income
-              </label>
+      {!isShowCreateForm ? (
+        <div className="my-3">
+          <button
+            className="btn btn-outline-warning w-100"
+            onClick={() => setIsShowCreateForm(true)}
+          >
+            Click here to create new transaction
+          </button>
+        </div>
+      ) : (
+        <div className="bg-white p-3 rounded-2 my-3">
+          <form className="row g-3">
+            {/* ********** Begin Radio Button: Expense or Income ********** */}
+            <div className="col-12">
+              <div className="btn-group">
+                <input
+                  type="radio"
+                  className="btn-check"
+                  id="cbx-expense"
+                  name="type"
+                  value="Expense"
+                  checked={type === "Expense"}
+                  onChange={(event) => {
+                    setType(event.target.value);
+                    setCategory(expenses[0]);
+                  }}
+                />
+                <label className="btn btn-outline-danger" htmlFor="cbx-expense">
+                  Expense
+                </label>
+                <input
+                  type="radio"
+                  className="btn-check"
+                  id="cbx-income"
+                  name="type"
+                  value="Income"
+                  checked={type === "Income"}
+                  onChange={(event) => {
+                    setType(event.target.value);
+                    setCategory(incomes[1]);
+                  }}
+                />
+                <label className="btn btn-outline-success" htmlFor="cbx-income">
+                  Income
+                </label>
+              </div>
             </div>
-          </div>
-          {/* ********** End Radio Button: Expense or Income ********** */}
+            {/* ********** End Radio Button: Expense or Income ********** */}
 
-          {/* ********** Begin Input: Payee ********** */}
-          <div className="col-sm-6">
-            <label className="form-label">Payee</label>
-            <input type="text" className="form-control" />
-          </div>
-          {/* ********** End Input: Payee ********** */}
-
-          {/* ********** Begin Select: Category ********** */}
-          <div className="col-sm-6">
-            <label className="form-label">Category</label>
-            <select className="form-select">
-              <option>Food</option>
-              <option>Investment</option>
-              <option>Salary</option>
-              <option>Transport</option>
-            </select>
-          </div>
-          {/* ********** End Select: Category ********** */}
-
-          {/* ********** Begin Input: Amount ********** */}
-          <div className="col-sm-6">
-            <label className="form-label">Amount</label>
-            <input type="text" className="form-control" />
-          </div>
-          {/* ********** End Input: Amount ********** */}
-
-          {/* ********** Begin Input: Date ********** */}
-          <div className="col-sm-6">
-            <label className="form-label">Date</label>
-            <input type="date" className="form-control" />
-          </div>
-          {/* ********** End Input: Date ********** */}
-
-          {/* ********** Begin Form Button ********** */}
-          <div className="col-12">
-            <div className="mt-3 d-flex gap-3">
-              <button className="btn btn-primary">Save</button>
-              <button className="btn btn-outline-secondary">Cancel</button>
-              <button className="btn btn-outline-danger">Delete</button>
+            {/* ********** Begin Input: Payee ********** */}
+            <div className="col-sm-6">
+              <label className="form-label">Payee</label>
+              <input
+                type="text"
+                className="form-control"
+                value={payee}
+                onChange={(event) => setPayee(event.target.value)}
+              />
             </div>
-          </div>
-          {/* ********** End Form Button ********** */}
-        </form>
-      </div>
+            {/* ********** End Input: Payee ********** */}
+
+            {/* ********** Begin Select: Category ********** */}
+            <div className="col-sm-6">
+              <label className="form-label">Category</label>
+              <select
+                className="form-select"
+                value={category}
+                onChange={(event) => setCategory(event.target.value)}
+              >
+                {type === "Expense"
+                  ? expenses.map((item, index) => (
+                      <option key={index} value={item}>
+                        {item}
+                      </option>
+                    ))
+                  : incomes.map((item, index) => (
+                      <option key={index} value={item}>
+                        {item}
+                      </option>
+                    ))}
+              </select>
+            </div>
+            {/* ********** End Select: Category ********** */}
+
+            {/* ********** Begin Input: Amount ********** */}
+            <div className="col-sm-6">
+              <label className="form-label">Amount</label>
+              <input
+                type="text"
+                className="form-control"
+                value={amount}
+                onChange={(event) => setAmount(event.target.value)}
+              />
+            </div>
+            {/* ********** End Input: Amount ********** */}
+
+            {/* ********** Begin Input: Date ********** */}
+            <div className="col-sm-6">
+              <label className="form-label">Date</label>
+              <input
+                type="date"
+                className="form-control"
+                value={date}
+                onChange={(event) => setDate(event.target.value)}
+              />
+            </div>
+            {/* ********** End Input: Date ********** */}
+
+            {/* ********** Begin Form Button ********** */}
+            <div className="col-12">
+              <div className="mt-3 d-flex gap-3">
+                <button className="btn btn-primary">Save</button>
+                <button
+                  type="button"
+                  className="btn btn-outline-secondary"
+                  onClick={() => setIsShowCreateForm(false)}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+            {/* ********** End Form Button ********** */}
+          </form>
+        </div>
+      )}
       {/* ********** End Create Form ********** */}
 
       {/* ********** Begin Transaction List ********** */}
